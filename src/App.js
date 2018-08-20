8b9fafad791c7093d6273d47b267dd625c078e0d
Terence Mangram <tmangram@gmail.com>
2018-08-19 19:18:40 -0500

    Testing modal and backdrop replacement for contact links

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Portfolio from "./components/Portfolio/";
 import Skills from "./components/Skills/";
 import Resume from "./components/Resume/";
 import Contact from "./components/Contact/";
+import Modal from "./components/UI/Modal";
+import Backdrop from "./components/UI/Backdrop";
 import Footer from "./components/Footer/";
 
 class App extends Component {
@@ -25,7 +27,9 @@ class App extends Component {
         <Portfolio />
         <Skills />
         <Resume />
-        <Contact show={this.state.show} handleClick={this.onButtonClick} />
+        {/*<Contact show={this.state.show} handleClick={this.onButtonClick} />*/}
+        {this.state.show && <Modal show={this.state.show} handleClick={this.onButtonClick} />}
+        {this.state.show && <Backdrop show={this.state.show} handleClick={this.onButtonClick} />}
         <Footer />
       </Fragment>
     );
